89caf415f6bbc6c36f28498ef5e5dd367cf0bf30
Muweee <ilaryia@yandex.ru>
2025-11-10 23:23:35 +0300

    Second stage:

--- Stage_1.py
+++ Stage_1.py
@@ -1,10 +1,12 @@
 import argparse
+import urllib.request
+import re
 
 class CLI_Ubuntu:
     def __init__(self):
         self.params = self.cmd_line()
-        self.print_args()
-
+        #self.print_args()
+        self.Ubuntu_packages()
 
     def cmd_line(self):
         params = {}
@@ -46,6 +48,69 @@ class CLI_Ubuntu:
         print(f"package-name:\t{self.params['package_name']}")
         print(f"url:\t\t{self.params['url']}")
         print(f"graph_name:\t{self.params['graph_name']}")
+    
+    def Ubuntu_packages(self):
+        url =  f"https://{self.params['url']}/{self.params['package_name']}"
+        print(f"https://{self.params['url']}/{self.params['package_name']}")
 
+        file = urllib.request.urlopen(url)
+        new_file = file.read()
+        # print(new_file)
+        new_file.decode("utf-8") #Битовый формат
 
+        error_found = b'<h1>Error</h1>' #регулярка в битовый
+        reg = re.search(error_found, new_file)
+        if reg != None:
+            print(f"Неккоректная ссылка")
+        else:
+
+            flag_stop = 0
+            find_uldep = b'<ul class="uldep">'
+            this_file = new_file
+            uldep_parts = this_file.split(find_uldep, -1) #разделение по регулярному выражению
+            find_ulrec = b'<ul class="ulrec">' #проверка на наличие рекомендаций
+            check = re.search(find_ulrec, uldep_parts[2])
+            if check != None:
+                uldep_parts_found = uldep_parts[2].split(find_ulrec, -1)
+                check_uldep = 0
+                flag_stop = 1
+            else:
+                check_uldep = 1
+                # В uldep_parts_found[0] хранятся нужные пакеты
+
+            if flag_stop == 0:
+                find_ulsug = b'<ul class="ulsug">'
+                check = re.search(find_ulsug, uldep_parts[2])
+                if check != None:
+                    uldep_parts_found = uldep_parts[2].split(find_ulsug, -1)
+                    check_uldep = 0
+                    flag_stop = 1
+                else:
+                    check_uldep = 1
+
+            if flag_stop == 0:
+                find_ulenh = b'<ul class="ulenh">'
+                check = re.search(find_ulenh, uldep_parts[2])
+                if check != None:
+                    uldep_parts_found = uldep_parts[2].split(find_ulenh, -1)
+                    check_uldep = 0
+                    flag_stop = 1
+                else:
+                    check_uldep = 1
+                    # В uldep_parts_found[0] хранятся пакеты необходимые для работы вводимого пакета
+
+            pattern = (b'>([a-zA-Z0-9_\\-\\+]*)</a>\n\t')
+            reg = re.compile(pattern)
+            if check_uldep == 0:
+                package = reg.findall(uldep_parts_found[0])
+            else:
+                package = reg.findall(uldep_parts[2])
+            i = 0
+            package_str = set()
+            while i < len(package):
+                package_str.add(str(package[i], encoding='utf-8'))
+                i = i + 1
+            print('Зависимые пакеты:', package_str)
+
+print("Package for example: aide/abyss \nURL format: packages.ubuntu.com/questing")
 CLI = CLI_Ubuntu()
